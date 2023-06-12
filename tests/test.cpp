f9a46289e6337c52f158e2e2427e5739e7ad5c29
drchen22 <875339353@qq.com>
2023-06-12 18:36:54 +0800

    finish json
    finish module json_parser

---
+++ tests/test.cpp
@@ -0,0 +1,42 @@
+//
+// Created by jc133 on 2023/6/11.
+//
+
+#include <catch2/catch_test_macros.hpp>
+#include <vector>
+
+
+TEST_CASE( "vectors can be sized and resized", "[vector]" ) {
+
+    // For each section, vector v is anew:
+
+    std::vector<int> v( 5 );
+
+    REQUIRE( v.size() == 5 );
+    REQUIRE( v.capacity() >= 5 );
+
+    SECTION( "resizing bigger changes size and capacity" ) {
+        v.resize( 10 );
+
+        REQUIRE( v.size() == 10 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "resizing smaller changes size but not capacity" ) {
+        v.resize( 0 );
+
+        REQUIRE( v.size() == 0 );
+        REQUIRE( v.capacity() >= 5 );
+    }
+    SECTION( "reserving bigger changes capacity but not size" ) {
+        v.reserve( 10 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 10 );
+    }
+    SECTION( "reserving smaller does not change size or capacity" ) {
+        v.reserve( 0 );
+
+        REQUIRE( v.size() == 5 );
+        REQUIRE( v.capacity() >= 5 );
+    }
+}
---
+++ tests/test_json.cpp
@@ -0,0 +1,9 @@
+//
+// Created by jc133 on 2023/6/12.
+//
+
+
+#include <catch2/catch_test_macros.hpp>
+#inc
+
+TEST_CASE
